871a0913d5db9ed3ae693cfc45d77b0cabe86552
lucasmanica <lucasmanica4@gmail.com>
2025-07-01 20:44:00 -0300

    [study] - Validation Form Init example

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,34 @@
 import React from 'react';
-import FormInputs from './Components/FormInputs';
-import FormSelect from './Components/FormSelect';
-import FormRadio from './Components/FormRadio';
-import FormCheckbox from './Components/FormCheckbox';
 const App = () => {
-  const [stateName, setStateName] = React.useState();
-  const [stateEmail, setStateEmail] = React.useState();
-  const [valueOption, setOption] = React.useState('');
-  const [valueRadio, setRadio] = React.useState('vermelho');
-  const [valueCheckboxes, setCheckboxes] = React.useState(['JavaScript']);
-  const [termo, setTermo] = React.useState([]);
+  const [cep, setCep] = React.useState('');
+  function validationCep() {
+    const regex = /^\d{5}-?\d{3}$/;
+    console.log(cep);
+    if (cep === 0) {
+      console.log(`Digite`);
+    } else if (regex.test(cep)) {
+      console.log(`opa cep valido`);
+    } else {
+      console.log(`digite um cep correto`);
+    }
+  }
+  function handleFunction(value) {
+    validationCep(value);
+  }
+  function setFunction({ target }) {
+    setCep(target.value);
+  }
   return (
     <div>
-      <h3>Termos</h3>
-      <FormCheckbox
-        options={['Concorda com os termos?']}
-        value={termo}
-        setValue={setTermo}
-      ></FormCheckbox>
-      <h3>Linguagens</h3>
-      <FormCheckbox
-        options={['JavaScript', 'PHP', 'Java', 'C#']}
-        value={valueCheckboxes}
-        setValue={setCheckboxes}
-      ></FormCheckbox>
-      <h3>Radio</h3>
-      <FormRadio
-        options={['azul', 'vermelho', 'rosa', 'laranja']}
-        value={valueRadio}
-        setValue={setRadio}
-      ></FormRadio>
-      <h3>Times</h3>
-      <FormSelect
-        options={['Fluminense', 'Flamengo', 'Palmeiras']}
-        value={valueOption}
-        setOption={setOption}
-      ></FormSelect>
       <form>
-        <FormInputs
-          id="name"
-          label="Name"
+        <input
+          label="CEP"
+          id="cep"
           type="text"
-          value={stateName}
-          setValue={setStateName}
-          required
-        />
-        <FormInputs
-          id="email"
-          label="Email"
-          type="email"
-          value={stateEmail}
-          setValue={setStateEmail}
-        />
+          value={cep}
+          onBlur={handleFunction}
+          onChange={setFunction}
+        ></input>
         <button>Enviar</button>
       </form>
     </div>
---
+++ src/Validation/AppValidation.jsx
@@ -0,0 +1,51 @@
+export const AppValidation = () => {
+  const [cep, setCep] = React.useState('');
+  const [error, setError] = React.useState(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (validateCep(cep)) {
+      console.log('Enviar');
+    } else {
+      console.log('Não enviar');
+    }
+  }
+
+  function validateCep(value) {
+    if (value.length === 0) {
+      setError('Preencha um valor');
+      return false;
+    } else if (!/^\d{5}-?\d{3}$/.test(value)) {
+      setError('Preencha um cep válido');
+      return false;
+    } else {
+      setError(null);
+      return true;
+    }
+  }
+
+  function handleBlur({ target }) {
+    validateCep(target.value);
+  }
+
+  function handleChange({ target }) {
+    if (error) validateCep(target.value);
+    setCep(target.value);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="cep">Cep</label>
+      <input
+        id="cep"
+        type="text"
+        value={cep}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        placeholder="00000-000"
+      />
+      {error && <p>{error}</p>}
+      <button>Enviar</button>
+    </form>
+  );
+};
